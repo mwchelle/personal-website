7be463d0bd3b2224727c676fdc89116842624823
Michelle <109537947+mwchelle@users.noreply.github.com>
2024-01-17 15:21:08 -0800

    commit

---
+++ portfoliosite/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Portfolio from "./Portfolio";
+import Skills from "./Skills";
+
+export default function Home() {
+    return (
+        <>
+        <Hero />
+        <About />
+        <Skills />
+        <Portfolio />
+        <Contact />
+        <Footer />
+        </>
+    )
+}
---
+++ portfoliosite/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import girlCode from "./pages/Projects/girlcode";
+import splytrUX from "./pages/Projects/splytr";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/girlcode" element={<girlCode />}></Route>
+            <Route path="/splytr" element={<splytrUX />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

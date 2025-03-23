10fac72b13369f059a97045622362b13e670f059
Michelle <mw.chellecx@gmail.com>
2025-03-23 16:54:56 -0400

    base formatting for projects

--- src/pages/component/Card.jsx
+++ src/pages/component/Card.jsx
@@ -1,19 +1,42 @@
-function Card(props){
+import { useState } from "react";
+import "./Card.css"; // Import the CSS file
+
+export default function Card({ title, titleColor, image, tags, link, description }) {
+    const [hovered, setHovered] = useState(false);
+
     return (
-        <div className="card">
-            <div className="card--content-box">
-                <div className="card--intro">
-                    <h3 className="card--type">{props.type}</h3>
+        <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-card"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            {/* Background Image */}
+            <div
+                className="background-image"
+                style={{ backgroundImage: `url(${image})` }}
+            ></div>
+
+            {/* Tags in the Top Right */}
+            <div className="tags">
+                {tags.map((tag, index) => (
+                    <span key={index} className="tag">{tag}</span>
+                ))}
+            </div>
+
+            {/* Project Title */}
+            <h2 className="project-title" style={{ color: titleColor }}>
+                {title} ↗
+            </h2>
+
+            {/* Hovered Description */}
+            {hovered && (
+                <div className="description">
+                    {description}
                 </div>
-                <img className="card--img" src={props.preview}/>
-                <div className="card--text">
-                    <center><em><h3 className="card--name">{props.name}</h3></em></center>
-                    <center><p className="card--desc">{props.desc}</p></center>
-                </div>
-                <button className="card--button">{props.button}</button>
-                </div>
-        </div>
+            )}
+        </a>
     );
 }
-
-export default Card
